b5d1850016eaf485fe620b743073650edadd4257
Dinujaya69 <kavinduweerasinha4@gmail.com>
2025-06-22 13:31:32 +0530

    change unwanted comments

--- controller/authController.js
+++ controller/authController.js
@@ -277,7 +277,7 @@ class AuthController {
 
       await TokenBlacklist.create({
         token: refreshToken,
-        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), 
         tokenType: "refresh",
       });
 
--- utils/otpService.js
+++ utils/otpService.js
@@ -10,7 +10,6 @@ const createTransporter = () => {
   });
 };
 
-// Fixed: Added return statement
 export const generateOTP = () => {
   return Math.floor(10000 + Math.random() * 90000).toString();
 };
